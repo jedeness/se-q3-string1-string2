480c567cb24bad30c360960fad01e3dece86782d
JED <enasjed@gmail.com>
2020-08-13 18:22:41 -0400

    All tests passed

--- string1.py
+++ string1.py
@@ -4,7 +4,7 @@ Kenzie assignment: String1
 """
 # Your name, plus anyone who helped you with this assignment.
 # Give credit where credit is due.
-__author__ = "???"
+__author__ = "Jed Enas"
 
 # Copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
@@ -28,8 +28,12 @@ __author__ = "???"
 
 
 def donuts(count):
-    # your code here
-    return
+    donuts_string = "Number of donuts: "
+    if count >= 10:
+        donuts_string += "many"
+    elif count < 10:
+        donuts_string += str(count)
+    return donuts_string
 
 
 # B. both_ends
@@ -42,8 +46,9 @@ def donuts(count):
 
 
 def both_ends(s):
-    # your code here
-    return
+    if len(s) < 2:
+        return ''
+    return s[:2] + s[-2:]
 
 
 # C. fix_start
@@ -58,8 +63,7 @@ def both_ends(s):
 
 
 def fix_start(s):
-    # your code here
-    return
+    return s[0] + s[1:].replace(s[0], '*')
 
 
 # D. mix_up
@@ -73,8 +77,7 @@ def fix_start(s):
 
 
 def mix_up(a, b):
-    # your code here
-    return
+    return b[:2] + a[2:] + ' ' + a[:2] + b[2:]
 
 
 # Provided simple test() function used in main() to print
